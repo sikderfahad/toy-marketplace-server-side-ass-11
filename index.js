c9933abeae31e15f8c030cd995b1d5e232c5c93a
unknown <sikderfahad>
2023-06-08 03:05:24 +0600

    make toy data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const data = require("./data.json");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Express is Running...!");
+  res.send("Toyes server is Running...!");
+});
+
+app.get("/toyes", (req, res) => {
+  res.send(data);
 });
 
 app.listen(port, () => {
