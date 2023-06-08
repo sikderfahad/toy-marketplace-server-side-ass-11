dee64a5380772757083f30fd62578027a9e6c914
unknown <sikderfahad>
2023-06-08 20:59:09 +0600

    delete routes developed

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE a user created Toy
+    app.delete("/alToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addUserToyCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
