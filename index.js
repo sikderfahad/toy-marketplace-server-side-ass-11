622598b3f2397efb78e629f3c2d6b3c714ae8d4c
unknown <sikderfahad>
2023-09-02 15:57:09 +0600

    some little changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 // const data = require("./data.json");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 console.log(process.env.DB_USER);
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3zuhxgd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
